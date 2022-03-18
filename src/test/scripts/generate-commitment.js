86bebadf55c9f9220457a14b42912f2a689cd526
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-03-18 03:40:19 +0100

    Generate commitment script

---
+++ src/test/scripts/generate-commitment.js
@@ -0,0 +1,15 @@
+const ethers = require("ethers");
+const { ZkIdentity, Strategy } = require("@zk-kit/identity");
+
+async function main() {
+	const identity = new ZkIdentity(Strategy.MESSAGE, "test-identity");
+
+	process.stdout.write(
+		ethers.utils.defaultAbiCoder.encode(
+			["uint256"],
+			[identity.genIdentityCommitment()]
+		)
+	);
+}
+
+main();
