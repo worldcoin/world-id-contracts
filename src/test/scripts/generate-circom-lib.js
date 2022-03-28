7f0e426b840a74e19c2f22dee1d581216a3eda88
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-03-28 20:40:25 +0200

    Add context to the circom script

--- src/test/scripts/generate-circom-lib.js
+++ src/test/scripts/generate-circom-lib.js
@@ -1,6 +1,9 @@
 import fs from 'fs'
 import { poseidon_gencontract } from 'circomlibjs'
 
+// Circom doesn't provide a Solidity implementation of the Poseidon library, and instead expects
+// you to generate one at runtime. To make this system work with Foundry, we first compile
+// the interface included in Semaphore, then inject the bytecode on the cache file.
 function main() {
     const poseidonT3Bytecode = poseidon_gencontract.createCode(2)
     const poseidonT3Abi = poseidon_gencontract.generateABI(2)
