cdf157d89a52196e82239ce6d1325c5d6787c946
Ara Adkins <iamrecursion@users.noreply.github.com>
2023-01-09 06:00:30 -0700

    Add fuzzing to the tests that were not previously fuzzed (#29)
    * Port changes from #27
    
    This imports the solidity-side changes from #27 such that they can be
    based upon when fuzzing the tests.
    
    * Add fuzzing to the existing tests.
    
    Any remaining tests that are not yet fuzzed/property-based will have
    fuzzing added with a slightly different method. This will come in
    subsequent commits where possible.
    
    * Add a mock for verification
    
    This also adds fuzz testing for the rest of the things that were not
    previously fuzz tested and can be tested in such a manner.
    
    * Self-review

---
+++ src/test/mock/SimpleVerifier.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import {ITreeVerifier} from "../../interfaces/ITreeVerifier.sol";
+
+/// @title Simple Verifier
+/// @author Worldcoin
+/// @notice A dumb verifier to make it easy to fuzz test successes and failures.
+contract SimpleVerifier is ITreeVerifier {
+    function verifyProof(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[1] memory input
+    ) external pure override returns (bool) {
+        return SimpleVerify.verifyProof(a, b, c, input);
+    }
+}
+
+library SimpleVerify {
+    function isValidInput(uint256 a) public pure returns (bool) {
+        return a % 2 == 0;
+    }
+
+    function verifyProof(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[1] memory input
+    ) public pure returns (bool) {
+        delete b;
+        delete c;
+        delete input;
+        return isValidInput(a[0]);
+    }
+
+    function calculateInputHash(
+        uint32 startIndex,
+        uint256 preRoot,
+        uint256 postRoot,
+        uint256[] calldata identityCommitments
+    ) public pure returns (bytes32 hash) {
+        bytes memory bytesToHash =
+            abi.encodePacked(startIndex, preRoot, postRoot, identityCommitments);
+
+        hash = keccak256(bytesToHash);
+    }
+}
