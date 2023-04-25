f334a111df7d43053002b19315467d201e7729a9
dcbuilder.eth <dcbuilder@protonmail.com>
2023-04-25 22:19:30 +0100

    fix(state bridge compatibility) (#91)

--- src/interfaces/IBridge.sol
+++ src/interfaces/IBridge.sol
@@ -6,4 +6,9 @@ interface IBridge {
     /// @dev Calls this method on the L1 Proxy contract to relay the latest root to all supported networks
     /// @param root The latest Semaphore root.
     function sendRootMultichain(uint256 root) external;
+
+    /// @notice Sets the root history expiry for OpWorldID (on Optimism) and PolygonWorldID (on Polygon)
+    /// @param expiryTime The new root history expiry for OpWorldID and PolygonWorldID
+    /// @dev gated by onlyWorldIDIdentityManager modifier
+    function setRootHistoryExpiry(uint256 expiryTime) external;
 }
